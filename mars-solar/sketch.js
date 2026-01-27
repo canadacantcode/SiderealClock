fa2a7bc7f3540c7c54e27c6ac42c05e409d8efd3
Cole Canada <colecanada7@gmail.com>
2026-01-27 13:46:59 -0500

    Update sketch.js

--- mars-solar/sketch.js
+++ mars-solar/sketch.js
@@ -1,78 +1,72 @@
 let aangle = 0;
-
 let bangle = 0;
 
 function setup() {
   angleMode(DEGREES);
   createCanvas(400, 400);
+  frameRate(15);
 }
 
 function draw() {
-  background(" rgb(28,28,46) ");
-  frameRate(15); //set frame rate to 15
-  sv = frameCount % 8;
-  if (sv > 41) {
-    sv = frameCount % -8;
-  }
-push();
+  background(28, 28, 46);
+
+  let sv = frameCount % 8;
+  if (sv > 41) sv = frameCount % -8;
+
+  // Sun
+  push();
   stroke("orange");
   strokeWeight(sv);
   fill("yellow");
   circle(200, 200, 50);
-  cloudOneX = frameCount % width;
 
   textSize(20);
   strokeWeight(1);
   stroke("black");
+  fill("yellow");
   text("Sun", 183, 205);
 
-  fill(255); //white text
-
+  fill(255);
+  noStroke();
   textSize(10);
-  strokeWeight(0);
   text(`mouseX: ${mouseX}, mouseY: ${mouseY}`, 20, 20);
-pop();
-  
-  
-push();
+  pop();
+
+  // Orbit + Mars spin
+  push();
   translate(200, 200);
-  if (aangle) {
-    rotate(aangle);
-  }
+  rotate(aangle);
 
   stroke("black");
   fill("orange");
   circle(75, 75, 25);
+
   stroke("red");
   strokeWeight(1);
   line(0, 0, 0, 100);
 
+  // Mars rotation line
   push();
-  translate(75,75)
-  rotate(bangle)
-  fill("red");
-  
+  translate(75, 75);
+  rotate(bangle);
+  stroke("red");
   line(0, 0, 0, 50);
-  
-  pop();  
-  
-  
+  pop();
+
+  // Label stays readable
   push();
-  translate(75,75)
-  rotate(-aangle)
+  translate(75, 75);
+  rotate(-aangle);
   translate(10, 10);
   textSize(20);
   fill("black");
   stroke("orange");
   strokeWeight(1);
   text("Mars", -30, -25);
-  
-
-  
-  pop();
-  
   pop();
 
- bangle = bangle +67;
-  aangle = aangle + 2;
+  pop();
+
+  bangle += 67;
+  aangle += 2;
 }
